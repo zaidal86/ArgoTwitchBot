c1af00b920597eda12fd437a8c2c0b09bc463482
zaidal86 <william-vivier@hotmail.fr>
2023-10-24 23:03:54 +0200

    add function for read link from osu Beatmaps

--- src/event/twitch/message.js
+++ src/event/twitch/message.js
@@ -2,12 +2,13 @@ import { sendMessageForLogs } from '../../utils/discordSendMessage.js';
 import { createCommands } from '../../commands/create.js';
 import { removeCommands } from '../../commands/remove.js';
 import { listenCommands } from '../../commands/listen.js';
+import { getBeatmaps } from '../../api/Osu/getBeatmap.js';
 import { editCommands } from '../../commands/edit.js';
 import { twitchClient } from '../../../main.js';
 
 const user = [];
 
-const sayHello = (channel, tags) => {
+const sayHello = (channel, tags, self) => {
     if (self) return;
     if (!user.includes(tags.username)) {
         user.push(tags.username);
@@ -21,8 +22,22 @@ const sayHello = (channel, tags) => {
     };
 };
 
+const linkOsu = async (channel, tags, message, self) => {
+    if (self) return;
+    const osuLinkRegex = /(https?:\/\/)?osu\.ppy\.sh\/(beatmapsets\/(\d+)#osu\/(\d+))/i;
+
+    const match = message.match(osuLinkRegex);
+
+    if (match) {
+        const lastNumber = match[4];
+        const string = await getBeatmaps(lastNumber);
+        if (string) return twitchClient.say(channel, string);
+    };
+};
+
 export const message = (channel, tags, message, self) => {
     sayHello(channel, tags, self);
+    linkOsu(channel, tags, message, self);
     sendMessageForLogs(message);
     if (self || !message.startsWith('!')) return;
 
